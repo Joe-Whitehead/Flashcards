e9e5c15a3b05523cbbfda17f6a89247ba45a1558
Joe <joe980@gmail.com>
2025-10-22 19:01:01 +0100

    Inital Data structure

---
+++ Data/Database.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+
+
+namespace Flashcards.Data
+{
+    internal class Database
+    {
+        private readonly IConfiguration _configuration;
+
+        public Database(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public SqlConnection GetConnection()
+        {
+            var connectionString = _configuration.GetConnectionString("FlashcardsDatabase");
+            return new SqlConnection(connectionString);
+        }
+    }
+}
