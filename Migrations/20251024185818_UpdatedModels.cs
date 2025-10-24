cd1a5e89594e56af90433d464bc76e59765ddd81
Joe Whitehead <joe980@gmail.com>
2025-10-24 20:00:37 +0100

    Updated Migrations

---
+++ Migrations/20251024185818_UpdatedModels.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Flashcards.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastReviewedAt",
+                table: "Flashcards",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "Flashcards",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastReviewedAt",
+                table: "Flashcards");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "Flashcards");
+        }
+    }
+}
